a592dfd1f0fe5909eb9d462592c40727da56b588
Lashwiga <goodevan7@gmail.com>
2021-07-17 11:09:15 -0500

    Added the Sliding Function (Star, Delete) 2nd Try

--- lib/models/NotificationScreenListTile.dart
+++ lib/models/NotificationScreenListTile.dart
@@ -1,6 +1,8 @@
+// TODO Implement this library.
 // NOTE: This file makes a custom Widget based off of the ExpansionTile widget
 
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class NotificationScreenListTile extends StatelessWidget {
   // require title and description parameters
@@ -12,33 +14,37 @@ class NotificationScreenListTile extends StatelessWidget {
   // build a custom ExpansionTile
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      title: Text(title,style: TextStyle(fontSize: 18),),
-      // children shows up when expanded
-      children: [
-        Align(
-          alignment: Alignment.topLeft,
 
-          child: new Row(
-            children: [
-
-              new Container(
-                height: 50.0,
-                width: 50.0,
-                child: IconButton(icon: Icon(Icons.star), onPressed: () {},),
-              ),
-              new Container(
-                height: 50.0,
-                width: 50.0,
-                child: IconButton(icon: Icon(Icons.cancel), onPressed: () {},),
-              ),
-
-            ],
-
+    return  Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      child: Container(
+        color: Colors.white,
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.indigoAccent,
+            child: Text('Pic'),
+            foregroundColor: Colors.white,
           ),
+          title: Text('Notification'),
+          subtitle: Text('Caption'),
         ),
+      ),
 
+      secondaryActions: <Widget>[
+        IconSlideAction(
+
+          color: Colors.black45,
+          icon: Icons.star,
+        ),
+        IconSlideAction(
+
+          color: Colors.red,
+          icon: Icons.delete,
+        ),
       ],
     );
+
+
   }
 }
